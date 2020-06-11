70989dcfd6795c1fe192ac8f8b39785702843bd6
Marikyuun <65990607+Marikyuun@users.noreply.github.com>
2020-06-11 12:29:09 +1200

    More code written

--- 2. Quiz Answering Box.py
+++ 2. Quiz Answering Box.py
@@ -24,46 +24,89 @@ class Quiz:
         # User Instructions (row 1)
         self.quiz_instructions_label = Label(self.quiz_frame,
                                              text = "Click on the button that "
-                                                    "contains the correct answer.",
+                                                    "contains the correct answer."
+                                                    "\n\nWhen you are ready press START.",
                                              font = "Arial 10 italic", wrap = 290,
                                              bg = background_color,
                                              padx = 10, pady = 10)
         self.quiz_instructions_label.grid(row = 1)
         
-        # Question (row 2)
-        self.quiz_question_label = Label(self.quiz_frame,
+        # Start Button (row 2)
+        self.quiz_start_button = Button(self.quiz_frame,
+                                        text = "START",
+                                        font = "Arial 16 bold", wrap = 290,
+                                        bg = "cyan",
+                                        padx = 10, pady = 10, command = self.question)
+        self.quiz_start_button.grid(row = 2)
+        
+    def question(self):
+        Question(self)
+        
+        
+class Question:
+    def __init__(self,partner):
+         
+        background_color = "cyan"
+        
+        # Question Frame
+        self.question_frame = Frame(bg = background_color,
+                                    pady = 10)
+         
+        # Question (row 1)
+        self.quiz_question_label = Label(self.question_frame,
                                          text = "Placeholder",
                                          font = "Arial 10", wrap = 290,
                                          bg = background_color,
                                          padx = 10, pady = 10)  # Reads question from file and replaces placeholder with the question later.
-        self.quiz_question_label.grid(row = 2)
-        
-        
-class Question:
-     
+        self.quiz_question_label.grid(row = 1)
      
         
 class Answer:
+    def __init__(self):
+        
+        # Answer 1 (row 3)
+        self.quiz_answer_1_button = Button(self.quiz_frame,
+                                           text = "Placeholder",
+                                           font = "Arial 10", wrap = 290,
+                                           bg = background_color,
+                                           padx = 10, pady = 10, command = self.answer_1)  # Reads one possible answer from file and replaces place holder with the answer later.
+        self.quiz_answer_1_button.grid(row = 3)
+        
+        # Answer 2 (row 4)
+        self.quiz_answer_2_button = Button(self.quiz_frame,
+                                           text = "Placeholder",
+                                           font = "Arial 10", wrap = 290,
+                                           bg = background_color,
+                                           padx = 10, pady = 10, command = self.answer_2)  # Reads one possible answer from file and replaces place holder with the answer later.
+        self.quiz_answer_2_button.grid(row = 4)
+        
+        # Answer 3 (row 5)
+        self.quiz_answer_1_button = Button(self.quiz_frame,
+                                           text = "Placeholder",
+                                           font = "Arial 10", wrap = 290,
+                                           bg = background_color,
+                                           padx = 10, pady = 10, command = self.answer_3)  # Reads one possible answer from file and replaces place holder with the answer later.
+        self.quiz_answer_1_button.grid(row = 5)
+        
+        # Answer 4 (row 6)
+        self.quiz_answer_1_button = Button(self.quiz_frame,
+                                           text = "Placeholder",
+                                           font = "Arial 10", wrap = 290,
+                                           bg = background_color,
+                                           padx = 10, pady = 10, command = self.answer_4)  # Reads one possible answer from file and replaces place holder with the answer later.
+        self.quiz_answer_1_button.grid(row = 6)    
     
-    # Answer 1 (row 3)
-    self.quiz_answer_1_button = Button(self.quiz_frame,
-                                       text = "Placeholder",
-                                       font = "Arial 10", wrap = 290,
-                                       bg = background_color,
-                                       padx = 10, pady = 10, command = self.answer_1)  # Reads one possible answer from file and replaces place holder with the answer later.
-    self.quiz_answer_1_button.grid(row = 3)    
-    
-    def answer_1:
+    #def answer_1:
+        #print("a")
         
+    #def answer_2:
+        #print("a")
         
-    def answer_2:
+    #def answer_3:
+        #print("a")
         
-        
-    def answer_3:
-        
-        
-    def answer_4:
-        
+    #def answer_4:
+        #print("a")
         
         
         
