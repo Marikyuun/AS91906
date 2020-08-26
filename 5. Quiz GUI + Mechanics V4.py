38f4bc1a80da469cb80aa1b7e09ede7e7c3d1411
Marikyuun <65990607+Marikyuun@users.noreply.github.com>
2020-08-26 15:28:36 +1200

    Scoring System Added

--- 5. Quiz GUI + Mechanics V4.py
+++ 5. Quiz GUI + Mechanics V4.py
@@ -26,7 +26,7 @@ class Quiz:
         # Quiz Heading (row 0)
         self.quiz_heading_label = Label(self.quiz_frame,
                                         text = "Geography Quiz",
-                                        font = "Arial 16 bold", bg = background_color,
+                                        font = "calibri 16 bold", bg = background_color,
                                         padx = 10, pady = 10)
         self.quiz_heading_label.grid(row = 0)
         
@@ -35,7 +35,7 @@ class Quiz:
                                              text = "Click on the button that "
                                                     "contains the correct answer."
                                                     "\n\nWhen you are ready press START.",
-                                             font = "Arial 10 italic", wrap = 290,
+                                             font = "calibri 10 italic", wrap = 290,
                                              bg = background_color,
                                              padx = 10, pady = 10)
         self.quiz_instructions_label.grid(row = 1)
@@ -43,7 +43,7 @@ class Quiz:
         # Start Button (row 2)
         self.quiz_start_button = Button(self.quiz_frame,
                                         text = "START",
-                                        font = "Arial 16 bold", wrap = 290,
+                                        font = "calibri 16 bold", wrap = 290,
                                         bg = "cyan",
                                         padx = 10, pady = 10, command = self.question)
         self.quiz_start_button.grid(row = 2)
@@ -67,6 +67,7 @@ class Question:
         for i in range(len(answers)):
             correct_answers.append(answers[i].split(",")[0])
         
+        print("The amount of remaining questions is: "+str(len(questions))+" and the amount of remaining answers is: "+str(len(answers)))
         print(questions)
         print(answers)
         print(correct_answers)
@@ -77,14 +78,17 @@ class Question:
         answer_control = temp_ans[0]
         print(questions[question_index])
         print(temp_ans)
-        print(answer)        
+        print(answer)
+        
         a_no=random.randint(0,3)
+        global a
         a=temp_ans[a_no]
         del temp_ans[a_no]
         if answer_control not in temp_ans:
             answer='a' #It deletes the random answer that was printed and checks the list to see if the answer is still in the list. If the answer has been deleted, then 'a' will be the answer. 
         
         b_no=random.randint(0,2)
+        global b
         b=temp_ans[b_no]
         del temp_ans[b_no]
         if answer!='a':
@@ -92,6 +96,7 @@ class Question:
                 answer='b' #It deletes the random answer that was printed and checks the list to see if the answer is still in the list. If the answer has been deleted, then 'b' will be the answer.            
         
         c_no=random.randint(0,1)
+        global c
         c=temp_ans[c_no]
         del temp_ans[c_no]
         if answer!='a' and answer!='b':
@@ -99,12 +104,13 @@ class Question:
                 answer='c' #It deletes the random answer that was printed and checks the list to see if the answer is still in the list. If the answer has been deleted, then 'c' will be the answer.            
         
         d_no=0
+        global d
         d=temp_ans[d_no]
         del temp_ans[d_no]
         if answer==answer_control:
-            answer='d' #If the answer isn't 'a' 'b' or 'c', the answer is 'd'.            
+            answer='d' #If the answer isn't 'a' 'b' or 'c', the answer is 'd'.
         
-        print(a,b,c,d)
+        print(answer)
         
         background_color = "cyan"
         
@@ -124,23 +130,24 @@ class Question:
             
         # Set up Question Heading (row 0)
         self.question_heading = Label(self.question_frame, text = "Question "+str(len(question_number)),
-                                      font = "arial 14 bold", bg = background_color, padx = 100, pady = 20, borderwidth = 5, relief = "solid")
-        self.question_heading.grid(row = 0)
-        
-        question_index = random.randint(0,len(questions)-1)            
+                                      font = "calibri 14 bold", bg = background_color, padx = 100, pady = 20, borderwidth = 5, relief = "solid")
+        self.question_heading.grid(row = 0)        
         
         # Question (row 1)
         self.question_label = Label(self.question_frame,
                                          text = questions[question_index],
-                                         font = "Arial 12 bold", wrap = 290,
+                                         font = "calibri 12 bold", wrap = 290,
                                          bg = "pale green",
                                          padx = 100, pady = 20)
         self.question_label.grid(row = 1)
         
+        del questions[question_index]
+        del answers[question_index]
+        
         # Answer 1 (row 2)
         self.question_answer_1_button = Button(self.question_frame,
                                            text = "a) "+a,
-                                           font = "Arial 10", wrap = 290,
+                                           font = "calibri 10", wrap = 290,
                                            bg = background_color,
                                            padx = 100, pady = 20, command = self.answer_1)
         self.question_answer_1_button.grid(row = 2, pady = 10)                   
@@ -148,7 +155,7 @@ class Question:
         # Answer 2 (row 3)
         self.question_answer_2_button = Button(self.question_frame,
                                            text = "b) "+b,
-                                           font = "Arial 10", wrap = 290,
+                                           font = "calibri 10", wrap = 290,
                                            bg = "coral",
                                            padx = 100, pady = 20, command = self.answer_2)
         self.question_answer_2_button.grid(row = 3, pady = 10)            
@@ -156,7 +163,7 @@ class Question:
         # Answer 3 (row 4)
         self.question_answer_3_button = Button(self.question_frame,
                                            text = "c) "+c,
-                                           font = "Arial 10", wrap = 290,
+                                           font = "calibri 10", wrap = 290,
                                            bg = background_color,
                                            padx = 100, pady = 20, command = self.answer_3)
         self.question_answer_3_button.grid(row = 4, pady = 10)            
@@ -164,14 +171,14 @@ class Question:
         # Answer 4 (row 5)
         self.question_answer_4_button = Button(self.question_frame,
                                            text = "d) "+d,
-                                           font = "Arial 10", wrap = 290,
+                                           font = "calibri 10", wrap = 290,
                                            bg = "coral",
                                            padx = 100, pady = 20, command = self.answer_4)
         self.question_answer_4_button.grid(row = 5, pady = 10)        
         
         self.next_question_button = Button(self.question_frame,
                                   text = "Next",
-                                  font = "Arial 10", wrap = 290,
+                                  font = "calibri 10", wrap = 290,
                                   bg = "white",
                                   padx = 100, pady = 20, command = self.next_question,
                                   state = DISABLED)
@@ -183,42 +190,58 @@ class Question:
             question_index = random.randint(0,len(questions)-1)
             temp_ans = answers[question_index].split(",")
             answer = temp_ans[0]
-            answer_control = temp_ans[0]                
+            answer_control = temp_ans[0]
+            
+            print("The amount of remaining questions is: "+str(len(questions))+" and the amount of remaining answers is: "+str(len(answers)))
+            print(questions[question_index])
+            print(temp_ans)
+            print(answer)
+            
             a_no=random.randint(0,3)
+            global a
             a=temp_ans[a_no]
             del temp_ans[a_no]
             if answer_control not in temp_ans:
                 answer='a' #It deletes the random answer that was printed and checks the list to see if the answer is still in the list. If the answer has been deleted, then 'a' will be the answer. 
             b_no=random.randint(0,2)
+            global b
             b=temp_ans[b_no]
             del temp_ans[b_no]
             if answer!='a':
                 if answer_control not in temp_ans:
                     answer='b' #It deletes the random answer that was printed and checks the list to see if the answer is still in the list. If the answer has been deleted, then 'b' will be the answer.            
             c_no=random.randint(0,1)
+            global c
             c=temp_ans[c_no]
             del temp_ans[c_no]
             if answer!='a' and answer!='b':
                 if answer_control not in temp_ans:
                     answer='c' #It deletes the random answer that was printed and checks the list to see if the answer is still in the list. If the answer has been deleted, then 'c' will be the answer.            
             d_no=0
+            global d
             d=temp_ans[d_no]
             del temp_ans[d_no]
+            
             if answer==answer_control:
                 answer='d' #If the answer isn't 'a' 'b' or 'c', the answer is 'd'.
+            
+            print(answer)
             
             self.next_question_button.config(state=DISABLED)
             self.question_answer_1_button.configure(text="a) "+a, state=NORMAL)
             self.question_answer_2_button.configure(text="b) "+b, state=NORMAL)
             self.question_answer_3_button.configure(text="c) "+c, state=NORMAL)
             self.question_answer_4_button.configure(text="d) "+d, state=NORMAL)
+            self.question_label.configure(text=questions[question_index])
             self.question_heading.configure(text=("Question "+str(len(question_number))))
+            del questions[question_index]
+            del answers[question_index]
+            
         else:
-            self.question_box.destroy()
             self.next_question_button.configure(command = self.score)
         
         def destroy_children(parent_frame):
-            # Destroy all childen from a frame
+            # Destroy all children from a frame
             # :param parent_frame: The frame to destory all children from
             
             for child in parent_frame.winfo_children():
@@ -234,40 +257,44 @@ class Question:
     
     
     def answer_1(self):
-        q='a'
+        if a in correct_answers:
+            scores.append("1")
+        print(len(scores))
         self.question_answer_1_button.configure(state=DISABLED)
         self.question_answer_2_button.configure(state=DISABLED)
         self.question_answer_3_button.configure(state=DISABLED)
         self.question_answer_4_button.configure(state=DISABLED)
         self.next_question_button.config(state=NORMAL)
-        print(q)
     
     def answer_2(self):
-        q='b'
+        if b in correct_answers:
+            scores.append("1")
+        print(len(scores))
         self.question_answer_1_button.configure(state=DISABLED)
         self.question_answer_2_button.configure(state=DISABLED)
         self.question_answer_3_button.configure(state=DISABLED)
         self.question_answer_4_button.configure(state=DISABLED)
         self.next_question_button.config(state=NORMAL)        
-        print(q)
     
     def answer_3(self):
-        q='c'
+        if c in correct_answers:
+            scores.append("1")
+        print(len(scores))
         self.question_answer_1_button.configure(state=DISABLED)
         self.question_answer_2_button.configure(state=DISABLED)
         self.question_answer_3_button.configure(state=DISABLED)
         self.question_answer_4_button.configure(state=DISABLED)
         self.next_question_button.config(state=NORMAL)        
-        print(q)
     
     def answer_4(self):
-        q='d'
+        if d in correct_answers:
+            scores.append("1")
+        print(len(scores))
         self.question_answer_1_button.configure(state=DISABLED)
         self.question_answer_2_button.configure(state=DISABLED)
         self.question_answer_3_button.configure(state=DISABLED)
         self.question_answer_4_button.configure(state=DISABLED)
-        self.next_question_button.config(state=NORMAL)        
-        print(q)
+        self.next_question_button.config(state=NORMAL)
     
     def close_question(self, parent):
         # Put start button back to normal...
